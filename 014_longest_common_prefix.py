ddb6aead3fa32481d38e9b93886e7f0005874ad4
Faisal Sajjad <illusionfs@gmail.com>
2021-06-23 11:57:53 -0700

    submitted easy 14

--- 014_longest_common_prefix.py
+++ 014_longest_common_prefix.py
@@ -34,34 +34,56 @@ strs = ["flower","flow","flight", "fles", "flor"]
 def longestCommonPrefix(strs):
 
     common=""
+    
+    # check if there is a null 
     if "" in strs:
         common=""
+    
+    # check if there is only one element in the array 
     elif len(strs)==1:
         common=strs[0]
+    
     else: 
+        # get the length of the shortest word(s)
         shortest_length=len(min((word for word in strs if word), key=len))
-
-        print(shortest_length)
+        print('shortest length word is of size:', shortest_length)
 
         # get all words of a certain length 
         shortest_length_strs=[word for word in strs if len(word) == shortest_length]
 
-        print(shortest_length_strs)
+        print('printing all words with shortest len', shortest_length_strs)
 
-        # get shortest word 
+        # chosing a random shortest word 
         shortest_length_word=shortest_length_strs[0]
-        print(shortest_length_word)
-        
+        print('chosing a random shortest word to play around with:', shortest_length_word)
+
         # loop over the shortest word 
         while shortest_length>0:
+
+            word_to_test=shortest_length_word[:shortest_length]
+            print('word is now:', word_to_test)
+
+            # keep total matches found 
+            total_matches_found=0
+
+
+            # looping over all the words 
             for i, n in enumerate(strs):
-                print('checking...', n)
-                if shortest_length_word[:shortest_length] in n:
-                    common=shortest_length_word[:shortest_length]
-                    print('common:',common)
+
+                print('checking word: ', n)
+                if word_to_test in n:
+                    print('full match found, going to the next word')
+                    common=word_to_test
+                    if total_matches_found<len(strs): 
+                        total_matches_found=total_matches_found+1
+                        print('total matches found:', total_matches_found)
+                    else:
+                        print('total matches found:', total_matches_found)
+                        #return common
                 else:
-                    print('didnt find breaking now', n)
+                    print('didnt find matching, breaking here: ', n)
                     common=""
+                    total_matches_found=0
                     break 
             shortest_length=shortest_length-1
     print('in the end:', common)
